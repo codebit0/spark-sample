9a3e66f4725edf2cfd1485fb7e7faf7dd29b8be9
bootcode <bootcode@218.38.137.27>
2017-12-06 16:15:05 +0900

    url read and workcount java code

---
+++ src/main/java/spark/sample/PairRDDCombineExample.java
@@ -0,0 +1,24 @@
+package spark.sample;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import scala.Tuple2;
+
+public class PairRDDCombineExample {
+	public static void main(String[] args) {
+		try(JavaSparkContext sc = SampleConfig.context()){
+			
+			//다운로드 파일 JavaPairRDD
+			String csv = scala.io.Source.fromURL("http://archive.ics.uci.edu/ml/machine-learning-databases/00398/dataset-CalheirosMoroRita-2017.csv", scala.io.Codec.ISO8859()).mkString();
+			JavaRDD<String> lines = sc.parallelize(Arrays.asList(csv.split("\\n")));
+			JavaPairRDD<String,Integer> nums = lines.mapToPair(s-> new Tuple2<String, Integer>(s.split(" ")[0], 1));
+			
+			
+		}
+	}
+}
--- src/main/java/spark/sample/WorkCount.java
+++ src/main/java/spark/sample/WorkCount.java
@@ -13,7 +13,13 @@ public class WorkCount {
 
 	public static void main(String[] args) {
 		try(JavaSparkContext sc = SampleConfig.context()){
-			JavaRDD<String> textFile = sc.textFile("hdfs://...");
+//			JavaRDD<String> textFile = sc.textFile("hdfs://...");
+			//JavaRDD<String> textFile = sc.textFile("http://archive.ics.uci.edu/ml/machine-learning-databases/00398/dataset-CalheirosMoroRita-2017.csv");
+			
+			//다운로드 파일 JavaPairRDD
+			String csv = scala.io.Source.fromURL("http://archive.ics.uci.edu/ml/machine-learning-databases/00398/dataset-CalheirosMoroRita-2017.csv", scala.io.Codec.ISO8859()).mkString();
+			List<String> lists = Arrays.asList(csv.split("\\n"));
+			JavaRDD<String> textFile = sc.parallelize(lists);
 			JavaPairRDD<String, Integer> counts = textFile
 			    .flatMap(s -> Arrays.asList(s.split(" ")).iterator())
 			    .mapToPair(word -> new Tuple2<>(word, 1))
